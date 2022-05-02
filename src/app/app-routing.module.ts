94454429a2f3c6f0b7faf6a8b15b57f798fa6d5b
danielmiquiles <danielvictormiquiles@gmail.com>
2022-05-02 16:39:47 -0300

    feat: configurando rotas inicias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./login/login.module').then( m => m.LoginModule),
+    },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
+import { LoginModule } from './login/login.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
