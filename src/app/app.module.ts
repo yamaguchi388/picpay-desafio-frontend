88a52c54f7d7f7241c632aa12d4625f0634e1a42
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2022-05-02 18:46:11 -0300

    Adicionando lib de UI Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { AccountsComponent } from './../pages/accounts/accounts.component';
 import { MyPaymentsComponent } from './../pages/my-payments/my-payments.component';
 import { AppRoutingModule } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [	
     AppComponent, AccountsComponent, MyPaymentsComponent,
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
