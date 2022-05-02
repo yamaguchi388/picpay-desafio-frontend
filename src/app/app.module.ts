21e2a5cae82eca3435f22560355c92f4bf2ba434
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2022-05-02 17:24:00 -0300

    Adiciona paginas ao arquivo de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
+import { MyPaymentsComponent } from './pages/my-payments/my-payments.component';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, AccountsComponent, MyPaymentsComponent,
    ],
   imports: [
     BrowserModule
