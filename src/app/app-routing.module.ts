b2cca0f617ca75bd93b1443aebb398db5054a2e2
Emerson Jahn <emersonajahn@gmail.com>
2022-05-04 20:43:56 -0300

    test(sign-in):  create test "should navigate to home when return a valid account from signIn"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'home', component: HomeComponent },
--- src/app/sign-in/sign-in.component.spec.ts
+++ src/app/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { SignInComponent } from './sign-in.component';
+import { AppService } from '../app.service';
+import { Account } from '../classes/Account';
+import { routes } from '../app-routing.module';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -8,7 +16,11 @@ describe('SignInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      imports: [
+        RouterTestingModule.withRoutes(routes), 
+        HttpClientTestingModule, ToastrModule.forRoot() 
+      ],
+      declarations: [ SignInComponent ],
     })
     .compileComponents();
   });
@@ -22,4 +34,28 @@ describe('SignInComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should navigate to home when return a valid account from signIn", fakeAsync(() => {
+    const account: Account = {
+      "id": 0,
+      "name": "usuario",
+      "email": "usuario@gaamail.com",
+      "password": "usuario"
+    }
+
+    const appService = fixture.debugElement.injector.get(AppService);
+    const spy = spyOn(appService, 'getAccountByEmailAndPassword').and.returnValue(Promise.resolve(account));
+
+    const router = fixture.debugElement.injector.get(Router);  
+
+    component.signIn();
+
+    fixture.detectChanges();
+    tick();
+
+    expect(appService.getAccountByEmailAndPassword).toHaveBeenCalled();
+    expect(spy).not.toThrow();
+    expect(router.url).toEqual('/home');
+  }));
+
 });
