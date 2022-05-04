aa5426b6125403603f066c46315d4be95932b0fc
Eduardo Drozda <edudrozda@gmail.com>
2022-05-04 19:58:06 -0300

    feat: created header of home module

--- src/app/modules/home/shared/components/header/header.component.ts
+++ src/app/modules/home/shared/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { IUser } from "src/app/shared/interfaces";
+import { UserService } from "src/app/shared/services/user/user.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  user!: IUser;
 
-  constructor() { }
+  constructor(private readonly userService: UserService) {}
 
   ngOnInit(): void {
+    this.getLoggedUser();
   }
 
+  private getLoggedUser() {
+    this.user = this.userService.getLoggedUser();
+  }
 }
