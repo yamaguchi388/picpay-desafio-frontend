952b406508a3ec80cb2cc122e20e30f44c13daca
Fillype Nascimento <fillypealna@gmail.com>
2022-05-04 14:50:05 -0300

    feat(auth): generates base scaffolding for auth features and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { PaymentsModule } from './features/payments/payments.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [	
@@ -13,6 +14,7 @@ import { PaymentsModule } from './features/payments/payments.module';
   imports: [
     BrowserModule,
     PaymentsModule,
+    CoreModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './authentication/components/login/login.component';
+import { ProfileComponent } from './authentication/views/profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    ProfileComponent
+  ],
   imports: [
     CommonModule
   ]
