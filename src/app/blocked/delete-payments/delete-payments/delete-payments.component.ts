93d75ead2abb761a79811587b40ad5f373d7d1ef
danielmiquiles <danielvictormiquiles@gmail.com>
2022-05-04 01:05:59 -0300

    feat: criando componente de exclusão

---
+++ src/app/blocked/delete-payments/delete-payments/delete-payments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-payments',
+  templateUrl: './delete-payments.component.html',
+  styleUrls: ['./delete-payments.component.scss']
+})
+export class DeletePaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
