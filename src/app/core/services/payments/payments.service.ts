bc95dd4e7ad64ccc885bc4b27f4632c0b5ada781
lvisentin <lucas@ancient.gg>
2022-05-13 12:50:14 -0300

    feat: new, delete and edit on payments service

--- src/app/core/services/payments/payments.service.ts
+++ src/app/core/services/payments/payments.service.ts
@@ -17,7 +17,27 @@ export class PaymentsService {
     return this.httpClient.get<Payment[]>(`${this.API_URL}/tasks`).pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
+  filterPaymentsByUsername(filterString: string): Observable<Payment[]> {
+    return this.httpClient
+      .get<Payment[]>(`${this.API_URL}/tasks?username_like=${filterString}`)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
+  }
+
   newPayment(payment: Payment): Observable<Payment> {
-    return this.httpClient.post<Payment>(`${this.API_URL}/tasks`, payment).pipe(catchError((err) => throwError(() => new Error(err))));
+    return this.httpClient
+      .post<Payment>(`${this.API_URL}/tasks`, payment)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
+  }
+
+  deletePayment(paymentId: string): Observable<Payment> {
+    return this.httpClient
+      .delete<Payment>(`${this.API_URL}/tasks/${paymentId}`)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
+  }
+
+  editPayment(payment: Payment): Observable<Payment> {
+    return this.httpClient
+      .put<Payment>(`${this.API_URL}/tasks/${payment.id}`, payment)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 }
