3aa4937a20429f71cd8cc621d48ee179187f18c1
tchfer <tchfernando@gmail.com>
2022-05-14 01:25:26 -0300

    update app component and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit{
-  title: string;
-
-  ngOnInit() {
-   this.title = 'Desafio Picpay Front-end';
-  }
+export class AppComponent implements OnInit {
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing-module';
+import { LoginModule } from './components/login/login.module';
+import { PaymentsModule } from './components/payments/payments.module';
+import { HeaderModule } from './shared/header/header.module';
+
+
 @NgModule({
   declarations: [	
     AppComponent,
    ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HeaderModule,
+    LoginModule,
+    PaymentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
