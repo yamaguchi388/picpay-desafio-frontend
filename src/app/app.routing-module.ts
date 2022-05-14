f4a2c5aa0222ffb1d1168f1cf9856e6c7683ba55
tchfer <tchfernando@gmail.com>
2022-05-14 01:24:45 -0300

    add routing module

---
+++ src/app/app.routing-module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { PaymentsComponent } from "./components/payments/payments.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'payments',
+    component: PaymentsComponent
+  }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
