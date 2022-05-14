78e4428609ea20661ff99652d7032d84f37bbf89
Tiago Israel <tiagoisrael77@hotmail.com>
2022-05-14 09:32:52 -0300

    fix: Corrigi falta de componente Modal
    Adição do componente ts Modal

---
+++ src/app/shared/components/modal/pp-modal.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
+
+@Component({
+  selector: "pp-modal",
+  templateUrl: "./pp-modal.component.html",
+  styleUrls: ["./pp-modal.component.scss"],
+})
+export class PpModalComponent implements OnInit {
+  @Input() title: string;
+  @ViewChild("modal") modalDiv: ElementRef<HTMLDivElement>;
+  isOpen = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public toggle(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  onClickOverlay(event: PointerEvent) {
+    const path = event.composedPath();
+    if(!path.some(el => (el as HTMLElement) === this.modalDiv.nativeElement)){
+      this.toggle();
+    }
+  }
+}
