20842c2a2d2edfb75bccbc1daa7f75f521531bf7
Tiago Israel <tiagoisrael77@hotmail.com>
2022-05-14 09:33:36 -0300

    test: Adiciona testes para a Modal
    Adição de testes unitários para a modal

--- src/app/shared/components/modal/pp-modal.component.spec.ts
+++ src/app/shared/components/modal/pp-modal.component.spec.ts
@@ -1,16 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from "@angular/core";
+import { ComponentFixture, TestBed, } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { PpModalComponent } from "./pp-modal.component";
 
-import { PpModalComponent } from './pp-modal.component';
 
-describe('PpModalComponent', () => {
+
+fdescribe("PpModalComponent", () => {
   let component: PpModalComponent;
   let fixture: ComponentFixture<PpModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PpModalComponent ]
-    })
-    .compileComponents();
+      declarations: [PpModalComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +21,34 @@ describe('PpModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render title", () => {
+    const title = "title";
+    component.title = title;
+    fixture.detectChanges();
+    const h2 = fixture.debugElement.query(By.css("h2"));
+
+    expect((h2.nativeElement as HTMLHeadingElement).textContent).toBe(title);
+  });
+
+  it("should open modal when toggle is triggered", () => {
+    component.toggle();
+    fixture.detectChanges();
+    const modalOverlay = fixture.debugElement.query(By.css(".overlay"));
+    const modal = modalOverlay.query(By.css(".modal"));
+    expect((modalOverlay.nativeElement as HTMLDivElement).classList).toContain("d-block");
+    expect(modal).toBeTruthy();
+  });
+
+  it("should close modal when toggle is triggered for the second time", () => {
+    component.toggle();
+    fixture.detectChanges();
+    component.toggle();
+    fixture.detectChanges();
+    const modalOverlay = fixture.debugElement.query(By.css(".overlay"));
+    expect((modalOverlay.nativeElement as HTMLDivElement).classList).toContain("d-none");
   });
 });
