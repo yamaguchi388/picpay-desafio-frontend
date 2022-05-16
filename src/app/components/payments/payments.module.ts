121658c986a1b675b976df0047d93da0ffbc8358
tchfer <tchfernando@gmail.com>
2022-05-16 04:13:11 -0300

    remove double quotes

--- src/app/components/payments/payments.module.ts
+++ src/app/components/payments/payments.module.ts
@@ -1,22 +1,22 @@
-import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-
-import { HeaderModule } from "./../../shared/header/header.module";
-import { FetchPaymentsComponent } from "./fetch-payments.component";
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { HeaderModule } from './../../shared/header/header.module';
 import { AddInsertPaymentsComponent } from './add-insert-payments/add-insert-payments.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DeletePaymentsComponent } from './delete-payments/delete-payments.component';
+import { FetchPaymentsComponent } from './fetch-payments.component';
 
 @NgModule({
-  declarations: [FetchPaymentsComponent, AddInsertPaymentsComponent],
+  declarations: [FetchPaymentsComponent, AddInsertPaymentsComponent, DeletePaymentsComponent],
   imports: [CommonModule,
     HeaderModule,
     MatButtonModule,
@@ -29,7 +29,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatDialogModule
+    MatDialogModule,
   ],
   exports: [FetchPaymentsComponent],
 })
