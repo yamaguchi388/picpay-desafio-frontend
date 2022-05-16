fd5c7be7d09a362176811a077dac78a7fe9014a8
Tiago Israel <tiagoisrael77@hotmail.com>
2022-05-15 21:20:37 -0300

    feat: Adiciona paginação na grid
    Funcionalidade de paginação.

--- src/app/features/my-payments/components/grid/pp-grid.component.ts
+++ src/app/features/my-payments/components/grid/pp-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Observable } from "rxjs";
+import { PaymentResponse } from "../../models/payment-response.model";
 import { Payment } from "../../models/payment.model";
 
 @Component({
@@ -8,12 +9,15 @@ import { Payment } from "../../models/payment.model";
   styleUrls: ["./pp-grid.component.scss"],
 })
 export class PpGridComponent implements OnInit {
-  @Input() data: Observable<Payment[]>;
+  @Input() data: PaymentResponse;
+  @Input() pageSize: number;
+  @Input() pageIndex: number;
   @Output() sort = new EventEmitter<{ sort: string; prop: string }>();
   @Output() edit = new EventEmitter<Payment>();
   @Output() delete = new EventEmitter<Payment>();
   @Output() search = new EventEmitter<string>();
-  @Output() pageSize = new EventEmitter<number>();
+  @Output("pageSize") pageSizeEvent = new EventEmitter<number>();
+  @Output("page-index") pageIndexEvent = new EventEmitter<number>();
   value: string = "";
 
   constructor() {}
@@ -29,7 +33,11 @@ export class PpGridComponent implements OnInit {
   }
 
   onPageSizeChange(page: number) {
-    this.pageSize.emit(page);
+    this.pageSizeEvent.emit(page);
+  }
+
+  onPageChanged(pageIndex: number) {
+    this.pageIndexEvent.emit(pageIndex);
   }
 
   onEdit(item: Payment) {
