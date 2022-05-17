c2baa36dcedddbcbba4f777a55ad43f74479c3e9
tchfer <tchfernando@gmail.com>
2022-05-17 12:42:08 -0300

    add default path and organize imports

--- src/app/app.routing-module.ts
+++ src/app/app.routing-module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { FetchPaymentsComponent } from "./components/payments/fetch-payments.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { FetchPaymentsComponent } from './components/payments/fetch-payments.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
     component: LoginComponent
   },
   {
