bb28e9a1ef9079fe5135f9f410a0bb99ae1456e8
tchfer <tchfernando@gmail.com>
2022-05-19 00:37:54 -0300

    finish auth service unit tests

--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,14 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import {
   HttpClientTestingModule,
-  HttpTestingController,
+  HttpTestingController
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
