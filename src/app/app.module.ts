dc7ec444e9ee50e20ec257c749d94911364484b2
t11199808440 <j.monteiro.ext@dasa.com.br>
2022-05-24 18:25:50 -0300

    criar páginas iniciais.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { PaymentsComponent } from "./pages/payments/payments.component";
+
+const appRoutes: Routes = [
+  { path: "entrar", component: LoginComponent },
+  { path: "pagamentos", component: PaymentsComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "**", component: NotFoundComponent },
+];
+
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-   ],
-  imports: [
-    BrowserModule
+    LoginComponent,
+    PaymentsComponent,
+    NotFoundComponent,
   ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
