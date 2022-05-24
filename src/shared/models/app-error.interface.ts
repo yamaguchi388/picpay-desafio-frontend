b50e283505758ca77d357c8d9f084fdea8e5d242
RafaelCaettano <rafac.docarmo@hotmail.com>
2022-05-24 10:09:25 -0300

    feat: criando models

---
+++ src/shared/models/app-error.interface.ts
@@ -0,0 +1,4 @@
+export interface AppError {
+    description: string;
+    where: string;
+}
---
+++ src/shared/models/tasks-index.interface.ts
@@ -0,0 +1,6 @@
+import { Task } from "./task.interface";
+
+export interface TasksIndex {
+    total: number;
+    tasks: Task[];
+}
