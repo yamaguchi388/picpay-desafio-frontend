0b1509865f05414826b484bd0634af8a1dace660
EduAraujoLima <edu.wp@outlook.com>
2022-05-27 19:28:07 -0300

    :sparkles: feat: NGXS configuration

--- src/app/core/state/state.module.ts
+++ src/app/core/state/state.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from '../../../environments/environment.prod';
 
 @NgModule({
   imports: [
+    NgxsLoggerPluginModule.forRoot(),
     NgxsModule.forRoot([], { developmentMode: !environment.production })
   ]
 })
