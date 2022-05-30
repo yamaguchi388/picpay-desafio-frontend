8e190f384db9f0c80e4146f77ce46b4f8cbb4011
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-05-29 22:54:24 -0300

    refactor: :recycle: atualiza auth guard e remove logged guard

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { CanLoad, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
@@ -7,10 +7,10 @@ import { AuthService } from '../../services/auth.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): Observable<boolean | UrlTree> {
+  canLoad(): Observable<boolean | UrlTree> {
     return this.authService.getUser().pipe(
       map((user) => !!user),
       tap((user) => {
