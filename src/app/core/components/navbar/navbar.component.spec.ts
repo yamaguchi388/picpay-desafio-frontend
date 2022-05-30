b442b2cc0b79edc40e05a7d56756faaf602b4cfa
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-05-30 19:05:16 -0300

    feat: :sparkles: implementa logout no navbar

--- src/app/core/components/navbar/navbar.component.spec.ts
+++ src/app/core/components/navbar/navbar.component.spec.ts
@@ -1,16 +1,28 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  const authService: jasmine.SpyObj<AuthService> =
+    jasmine.createSpyObj<AuthService>('AuthService', {
+      logout: undefined,
+    });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
+      declarations: [NavbarComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+      ],
+      providers: [{ provider: AuthService, useValue: authService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +33,11 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should do logout', () => {
+    spyOn(component['authService'], 'logout');
+    component.logout();
+    expect(component['authService'].logout).toHaveBeenCalled();
   });
 });
