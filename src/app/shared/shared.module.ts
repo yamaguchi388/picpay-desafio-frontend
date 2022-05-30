121d8facb3a7656d7ed5d8f8d45fa40da0dffe1d
Rafaela Queiroz <rqueiroz2108@gmail.com>
2022-05-30 19:03:01 -0300

    feat: :sparkles: atualiza shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSnackBarModule],
 })
 export class SharedModule {}
